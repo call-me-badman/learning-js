49eeeeb0cfbd3c6ae6933f0e2c94a8d52a5a98f7
Irakoze Olga <irakozeolga490@gmail.com>
2026-03-05 21:30:46 +0200

    Reset branch to only include EXPRESS folder

---
+++ REACT/pages/home.jsx
@@ -0,0 +1,16 @@
+import { link } from "react-router-dom";
+import "styles/home.css";
+const landingPage = () => {
+    return (
+        <div classname = "lp">
+            <main classname = "lp-main">
+                <section classname = "lp-hero">
+                    <div classname = "lp-hero-inner">
+                        <span></span>
+                    </div>
+                </section>
+            </main>
+        </div>
+    
+    )
+}
