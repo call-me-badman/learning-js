0ec5a68172dc3c1fbe88e050ecfedd2c4f25789a
Irakoze Olga <irakozeolga490@gmail.com>
2026-03-07 21:33:07 +0200

    Restored and updated CRUD apis using MongoDB

---
+++ EXPRESS/book.js
@@ -0,0 +1,11 @@
+const mongoose = require("./mongodb");
+
+const bookSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    year: Number
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
